06908c4e75f012499cc64ddee95e9df3813b7a75
DarushDev <darushfaqiri@gmail.com>
2017-09-01 03:09:08 +0800

    Find the number of clicks for all the answers of a question

--- app/src/main/java/com/example/myandroidcouchbase/QuestionActivity.java
+++ app/src/main/java/com/example/myandroidcouchbase/QuestionActivity.java
@@ -14,12 +14,18 @@ import android.widget.TextView;
 import com.couchbase.lite.Attachment;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Document;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
 import com.couchbase.lite.Revision;
+import com.couchbase.lite.support.LazyJsonArray;
 import com.example.myandroidcouchbase.adapter.QuestionOptionsAdapter;
 import com.example.myandroidcouchbase.model.Answer;
 import com.example.myandroidcouchbase.model.Question;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -50,12 +56,20 @@ public class QuestionActivity extends AppCompatActivity {
         mQuestion = ModelHelper.modelForDocument(document, Question.class);
         mTextView.setText(mQuestion.getText());
 
+        Query answers = Answer.getAnswersForQuestion(manager.database, mQuestion.get_id());
+        QueryEnumerator answersQuery = null;
+        try {
+            answersQuery = answers.run();
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+
         mQuestionOptions = (GridView) findViewById(R.id.question_options);
         mQuestionOptions.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
         mQuestionOptions.setNumColumns(2);
         mQuestionOptions.setSelector(R.drawable.selector_button);
 
-        mQuestionOptions.setAdapter(new QuestionOptionsAdapter(mQuestion.getOptions(), null));
+        mQuestionOptions.setAdapter(new QuestionOptionsAdapter(mQuestion.getOptions(), getAnswerCounts(answersQuery)));
 
         mQuestionOptions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -83,4 +97,22 @@ public class QuestionActivity extends AppCompatActivity {
         Answer answer = new Answer(mQuestion.get_id(), "answer", mQuestion.getOptions().get(mSelectedOption));
         ModelHelper.save(DataManager.getSharedInstance(getApplicationContext()).database, answer);
     }
+
+    private Map<String,Integer> getAnswerCounts(QueryEnumerator answers) {
+
+        Map<String,Integer> answerCounts = new HashMap<String, Integer>();
+
+        for (String option: mQuestion.getOptions()) {
+            answerCounts.put(option, 0);
+        }
+
+        for (QueryRow row : answers) {
+            LazyJsonArray<Object> key = (LazyJsonArray<Object>) row.getKey();
+            String answer = (String) key.get(1);
+            answerCounts.put(answer, (Integer)row.getValue());
+        }
+
+        return answerCounts;
+    }
+
 }
