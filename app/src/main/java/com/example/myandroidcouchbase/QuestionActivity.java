d3799fe360923bf1e3b9a47662a27d7596d3824a
DarushDev <darushfaqiri@gmail.com>
2017-09-01 00:11:53 +0800

    Show list of answers in the question activity where the user can choose a single answer

--- app/src/main/java/com/example/myandroidcouchbase/QuestionActivity.java
+++ app/src/main/java/com/example/myandroidcouchbase/QuestionActivity.java
@@ -3,11 +3,15 @@ package com.example.myandroidcouchbase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.couchbase.lite.Document;
+import com.example.myandroidcouchbase.adapter.QuestionOptionsAdapter;
 import com.example.myandroidcouchbase.model.Question;
 
 public class QuestionActivity extends AppCompatActivity {
@@ -38,6 +42,20 @@ public class QuestionActivity extends AppCompatActivity {
         // 3
         mQuestion = ModelHelper.modelForDocument(document, Question.class);
         mTextView.setText(mQuestion.getText());
+
+        mQuestionOptions = (GridView) findViewById(R.id.question_options);
+        mQuestionOptions.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        mQuestionOptions.setNumColumns(2);
+        mQuestionOptions.setSelector(R.drawable.selector_button);
+
+        mQuestionOptions.setAdapter(new QuestionOptionsAdapter(mQuestion.getOptions(), null));
+
+        mQuestionOptions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mSelectedOption = position;
+            }
+        });
 
     }
 
